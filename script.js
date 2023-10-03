5bb89c5e802e2a529228ec4ec036d78a4ac91c59
SakuSaku <travispastrana075@gmail.com>
2023-10-03 00:15:23 -0500

    cambios en el script

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const gatos = document.querySelectorAll('.gato');
 function cambiarColorDeFondo(event) {
     const seccionGato = event.currentTarget;
     const colores = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#F600FE', '#00FEE3 '];
-
     const nuevoColor = colores[Math.floor(Math.random() * colores.length)];
     seccionGato.style.transition = 'background-color 1.5s ease';
     seccionGato.style.backgroundColor = nuevoColor;
@@ -12,6 +11,24 @@ function cambiarColorDeFondo(event) {
 
 gatos.forEach((gato) => {
     gato.addEventListener('click', cambiarColorDeFondo);
+});
+
+const encabezado = document.querySelectorAll('li');
+
+// Función para cambiar el color de fondo de las secciones de encabezado al hacer clic en ellas
+function cambiarColor(event) {
+    const texto = event.currentTarget;
+    const colores = ['#FF5733', '#33FF57', '#5733FF', '#a2a20d', '#F600FE'];
+
+    const nuevoColor = colores[Math.floor(Math.random() * colores.length)];
+    texto.style.transition = 'background-color 1.5s ease';
+    texto.style.borderRadius = '4px';
+    texto.style.backgroundColor = nuevoColor;
+    texto.style.padding = '8px';
+}
+
+encabezado.forEach((encabezados) => {
+    encabezados.addEventListener('click', cambiarColor);
 });
 
 // Función para agregar un comentario a una sección de gato
